068a6031bc7e8acee03711e3d5277f1aa475b8a3
sbbbb-com <17719357702@163.com>
2021-08-11 16:35:23 +0800

    getForObject方法/getForEntity方法 比较

--- could-consumer-order80/src/main/java/com/yyesw/springcloud/controller/OrderController.java
+++ could-consumer-order80/src/main/java/com/yyesw/springcloud/controller/OrderController.java
@@ -4,6 +4,7 @@ import com.yyesw.springcloud.entities.CommonResult;
 import com.yyesw.springcloud.entities.Payment;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -42,6 +43,25 @@ public class OrderController {
         log.info("OrderController接收到的参数 id：{}",id);
 
         String url=PAYMENT_URL+"/payment/get/"+id;
+        /**
+         * 返回对象为响应体中数据转化成的对象，基本上可以理解为Json
+         */
         return restTemplate.getForObject(url,CommonResult.class);
+    }
+
+    @GetMapping("/consumer/payment/getForEntity/{id}")
+    public CommonResult<Payment> getPayment2(@PathVariable("id")Long id){
+
+        String url=PAYMENT_URL+"/payment/get/"+id;
+        /**
+         * 返回对象为ResponseEntity对象，包含了响应中的一些重要信息，比如响应头、响应状态码、响应体等
+         */
+        ResponseEntity<CommonResult> entity=restTemplate.getForEntity(url,CommonResult.class);
+
+        if (entity.getStatusCode().is2xxSuccessful()){
+            return entity.getBody();
+        }else {
+            return new CommonResult(444,"操作失败");
+        }
     }
 }
