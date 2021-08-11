107889f2fec5a8d94ba4c375684c804147cbb677
sbbbb-com <17719357702@163.com>
2021-08-11 15:43:32 +0800

    1.使用 consul 消费者服务 注册进consul 2.恢复之前的Eureka  等配套微服务为集群状态

---
+++ cloud-consumer-consul-order80/src/main/java/com/yyesw/springcloud/OrderConsulMain80.java
@@ -0,0 +1,20 @@
+package com.yyesw.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author hexinxiang
+ * <br/>Date: 2021/8/11
+ * <br/>Time: 14:55
+ */
+
+@SpringBootApplication
+@EnableDiscoveryClient  ///该注解用于向使用consul或者zookeeper作为注册中心时注册服务
+public class OrderConsulMain80 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderConsulMain80.class,args);
+    }
+}
---
+++ cloud-consumer-consul-order80/src/main/java/com/yyesw/springcloud/controller/OrderConsulController.java
@@ -0,0 +1,32 @@
+package com.yyesw.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
+import java.util.UUID;
+
+/**
+ * @author hexinxiang
+ * <br/>Date: 2021/8/11
+ * <br/>Time: 14:59
+ */
+
+@RestController
+public class OrderConsulController {
+
+    public static final String INVOME_URL = "http://consul-provider-payment";
+
+    @Resource
+    private RestTemplate restTemplate;
+
+    @GetMapping("/consumer/payment/consul")
+    public String payment (){
+        String result = restTemplate.getForObject(INVOME_URL+"/payment/consul",String.class);
+        return result;
+    }
+
+
+}
