ee2103e59d47111fe56da45a8d96407b1663b9d4
sbbbb-com <17719357702@163.com>
2021-08-13 10:50:15 +0800

    gateway  (服务9527) 网关基础环境搭建 测试成功

---
+++ cloud-gateway-gateway9527/src/main/java/com/yyesw/springcloud/GateWayMain9527.java
@@ -0,0 +1,20 @@
+package com.yyesw.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author hexinxiang
+ * <br/>Date: 2021/8/13
+ * <br/>Time: 10:34
+ */
+
+@EnableEurekaClient
+@SpringBootApplication
+public class GateWayMain9527 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GateWayMain9527.class,args);
+    }
+}
