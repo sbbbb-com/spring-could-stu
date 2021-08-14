878b78d747b0bf49278fc0e08fc448337e06ae43
sbbbb-com <17719357702@163.com>
2021-08-14 22:32:05 +0800

    stream: 消息驱动 消费者 实现 运行，【关闭健康检查】

---
+++ cloud-stream-rabbitmq-consumer8802/src/main/java/com/yyesw/springcloud/StreamMQMain8802.java
@@ -0,0 +1,19 @@
+package com.yyesw.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author hexinxiang
+ * <br/>Date: 2021/8/14
+ * <br/>Time: 21:32
+ */
+
+@SpringBootApplication
+public class StreamMQMain8802 {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(StreamMQMain8802.class,args);
+    }
+}
