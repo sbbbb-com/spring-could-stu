df97af03f8458e6162822a3b267d261bb390ca7a
sbbbb-com <17719357702@163.com>
2021-08-14 11:52:53 +0800

    使用注解 加运维发送 curl -X POST "http://localhost:3355/actuator/refresh" 方式 实现刷新配置

--- cloud-config-client-3355/src/main/java/com/yyesw/springcloud/controller/ConfigClientController.java
+++ cloud-config-client-3355/src/main/java/com/yyesw/springcloud/controller/ConfigClientController.java
@@ -1,6 +1,7 @@
 package com.yyesw.springcloud.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
  * <br/>Time: 9:32
  */
 
+@RefreshScope  //刷新的注解标签
+                /**
+                 * 同时运维同学 发生post请求 刷新 3355
+                 * 例如使用curl C:\Users\>curl -X POST "http://localhost:3355/actuator/refresh"
+                 */
 @RestController
 public class ConfigClientController {
 
